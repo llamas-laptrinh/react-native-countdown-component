79612f445fcde06d774a15ba18c3ec21dccc6a2b
lammas-laptrinh <danghoangson51@gmail.com>
2023-07-31 11:36:14 +0700

    update appstate

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class CountDown extends React.Component {
     onPress: PropTypes.func,
     onFinish: PropTypes.func,
   };
-
+  appStateSubscription;
   state = {
     until: Math.max(this.props.until, 0),
     lastUntil: null,
@@ -51,12 +51,14 @@ class CountDown extends React.Component {
   }
 
   componentDidMount() {
-    AppState.addEventListener('change', this._handleAppStateChange);
+    // AppState.addEventListener('change', this._handleAppStateChange);
+    this.appStateSubscription = AppState.addEventListener('change', this._handleAppStateChange);
   }
 
   componentWillUnmount() {
     clearInterval(this.timer);
-    AppState.removeEventListener('change', this._handleAppStateChange);
+    // AppState.removeEventListener('change', this._handleAppStateChange);
+    this.appStateSubscription.remove()
   }
 
   componentDidUpdate(prevProps, prevState) {
